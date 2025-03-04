80b6ba8fc8e0966105b4ef62c58e3c44e8945140
Lawer Nyako <86574038+lawaddle@users.noreply.github.com>
2025-03-04 06:20:40 -0500

    why no work good (i did the movement through)

---
+++ Team7_TurkishIceCream/Assets/Scripts/EnergyValue.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyValue : MonoBehaviour
+{
+    public int energy;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
